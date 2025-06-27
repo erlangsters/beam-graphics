f987c98abbf13567404867c201131f83df87196a
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2025-06-27 13:21:02 +0800

    Add placeholder files to sketch the API

---
+++ src/3d/mesh3.erl
@@ -0,0 +1,17 @@
+-module(mesh3).
+-moduledoc """
+To be written.
+
+To be written.
+""".
+
+-export([foobar/0]).
+
+-doc """
+To be written.
+
+To be written.
+""".
+-spec foobar() -> integer().
+foobar() ->
+    42.
