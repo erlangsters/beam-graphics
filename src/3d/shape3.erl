fc11121f3050513b4d476bf3a70af200c79399bf
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2025-08-05 22:41:54 +0800

    Implement point/2 line/3 and triangle/4 in shape3.erl

--- src/3d/shape3.erl
+++ src/3d/shape3.erl
@@ -19,10 +19,20 @@ To be written.
     with_meshes/1, with_meshes/2,
     destroy/1
 ]).
+-export([
+    point/2,
+    line/3,
+    triangle/4
+]).
 
 -compile({inline, [
     with_mesh/3, with_mesh/4,
     with_meshes/1, with_meshes/2
+]}).
+-compile({inline, [
+    point/2,
+    line/3,
+    triangle/4
 ]}).
 
 -include_lib("beam_graphics/include/graphics.hrl").
@@ -101,3 +111,53 @@ destroy(#shape3{meshes = Meshes}) ->
     lists:foreach(fun({Mesh, _PrimitiveType, _VertexCount}) ->
         mesh3:destroy(Mesh)
     end, Meshes).
+
+-doc """
+To be written.
+
+To be written.
+""".
+-spec point(graphics:vector3(), graphics:color()) -> graphics:shape3().
+point(Position, Color) ->
+    {ok, Mesh} = mesh3:with_vertices([?VERTEX3(Position, Color)]),
+    shape3:with_mesh(Mesh, points, 1).
+
+-doc """
+To be written.
+
+To be written.
+""".
+-spec line(
+    graphics:vector3(),
+    graphics:vector3(),
+    graphics:color()
+) ->
+    graphics:shape3()
+.
+line(From, To, Color) ->
+    {ok, Mesh} = mesh3:with_vertices([
+        ?VERTEX3(From, Color),
+        ?VERTEX3(To, Color)
+    ]),
+    shape3:with_mesh(Mesh, lines, 2).
+
+-doc """
+To be written.
+
+To be written.
+""".
+-spec triangle(
+    graphics:vector3(),
+    graphics:vector3(),
+    graphics:vector3(),
+    graphics:color()
+) ->
+    graphics:shape3()
+.
+triangle(A, B, C, Color) ->
+    {ok, Mesh} = mesh3:with_vertices([
+        ?VERTEX3(A, Color),
+        ?VERTEX3(B, Color),
+        ?VERTEX3(C, Color)
+    ]),
+    shape3:with_mesh(Mesh, triangles, 3).
